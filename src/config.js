b30c9ec69167498534a4ad5879ca295fd0312574
Gabriel <143111743+gdev15@users.noreply.github.com>
2025-04-16 16:06:41 -0500

    - Updated deployment URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // For deployment
-// export const API_BASE_URL = 'https://game-buddy-app.onrender.com';
+export const API_BASE_URL = 'https://game-buddy-app.onrender.com';
 // For Local Use
-export const API_BASE_URL = 'http://localhost:5000';
+// export const API_BASE_URL = 'http://localhost:5000';
